bbe12943589f9bc1d75ac17f3d0d667a7fff9ec1
Howard Tyson <howard@howardtyson.com>
2013-04-28 20:52:54 -0400

    a ton of work.  this is very bad VCS abuse.

--- client.js
+++ client.js
@@ -1,15 +1,23 @@
 
-var RText     = require('r-edit')
-var reconnect = require('reconnect')
-var reloader  = require('client-reloader')
-var widget    = require('r-edit/widget')
-var marked    = require('marked')
+var RText     = require('r-edit');
+var reconnect = require('reconnect');
+var reloader  = require('client-reloader');
+var widget    = require('r-edit/widget');
+var marked    = require('marked');
+var MuxDemux = require("mux-demux");
+
+/*
+var shoe = require("mux-demux-shoe")
+    , mdm = shoe("/shoe")
+*/
 
 var rText = RTEXT = RText()
 
 reconnect(reloader(function (stream) {
-  stream.pipe(rText.createStream()).pipe(stream)
-})).connect('/shoe');
+  var mx = MuxDemux();
+  console.log(title);
+  stream.pipe(rText.createStream()).pipe(stream);
+})).connect('/shoe/');
 
 //using the default template...
 textArea = rText.widget()
--- server.js
+++ server.js
@@ -1,30 +1,48 @@
+// We use the mux-demux-shoe to to support multiple channels.
 var shoe     = require('shoe');
 var ecstatic = require('ecstatic');
 var join     = require('path').join;
 var reloader = require('client-reloader');
+var hbs      = require('hbs');
+var REdit    = require('r-edit');
+var MuxDemux = require('mux-demux');
 
 var express = require('express');
 var app = express();
 
-var rText = require('r-edit')();
-rText.push('open in multiple tabs and start editing!');
+var openPages = {};
 
 var PORT = process.env.PORT || 3000;
 
-app.get('/pages/:title', function(req, res) {
-  res.writeHead(200);
-  console.log(req.params.something);
-  res.end('loaded');
-});
+function getRtext(name) {
+  if (!openPages[name]) {
+    openPages[name] = REdit();
+    openPages[name].push('# ' + name);
+  }
+  return openPages[name];
+}
 
 app.use(app.router);
 app.use(ecstatic(join(__dirname, 'static')));
+app.set('view engine', 'hbs');
+
+app.get('/pages/:title', function(req, res) {
+  // res.writeHead(200);
+  var title = req.params.title;
+  // getRtext(title);
+  console.log(title);
+  res.render('index', {title: title});
+});
 
 var shoeHandler = reloader(function (stream) {
-  stream.pipe(rText.createStream()).pipe(stream)
-  // console.log(rText.text());
-  // log the current text each time data comes through.
-  stream.on('data', function() {console.log(rText.text())});
+  stream.on('connection', function() {
+    console.log('pancakebatter')
+  });
+  stream.pipe(getRtext('foo').createStream()).pipe(stream)
+  // stream.pipe(MuxDemux(function (_stream) {
+  //   console.log(_stream);
+  //   // stream.pipe(getRtext().createStream()).pipe(stream)
+  // }));
 });
 
 var server = app.listen(PORT, function () {
