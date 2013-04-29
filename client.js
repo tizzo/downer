dd7fae12254d3ecb86394d1875f16367f9baf35d
Howard Tyson <howard@howardtyson.com>
2013-04-29 11:54:45 -0400

    Cleaning up client side code, making markdown updates evented.

--- client.js
+++ client.js
@@ -6,10 +6,6 @@ var widget    = require('r-edit/widget');
 var marked    = require('marked');
 var MuxDemux = require("mux-demux");
 
-/*
-var shoe = require("mux-demux-shoe")
-    , mdm = shoe("/shoe")
-*/
 
 var rText = RTEXT = RText()
 
@@ -17,30 +13,45 @@ var rText = RTEXT = RText()
 // `reconnect()` and `reloader()` reconnect the stream in the event
 // of an interruption (network or otherwise).
 reconnect(reloader(function (stream) {
+
   // We create a new instance of MuxDemux() for each connection.
   var mdm = MuxDemux()
+
   // We create the read/write stream to interface with rText().
   var rtStream = rText.createStream();
+  rtStream.on('data', updateMarkdown);
+
   // We connect our shoe stream directly our muxer/demuxer.
   // This allows the muxer to encode anything written to any virtual connection
   // multiplexed through the one stream and the demuxer to decode anything coming
   // back from the server.
   stream.pipe(mdm).pipe(stream);
+
   // Create an individual channel on the main read/write stream.
   var mxdTitleStream = mdm.createStream(title)
+
   // Pipe anything sent on this channel into the rtext stream and pipe anything
   // coming back from that stream back across the virtual connection.
   mxdTitleStream.pipe(rtStream).pipe(mxdTitleStream)
+
 })).connect('/shoe/');
 
-//using the default template...
-textArea = rText.widget()
-$('#text').append(textArea);
-$textArea = $('#text textarea');
-var updateMarkdown = function() {
-  $('#markdown').html(marked($textArea.val()));
+  // Using the default widget.
+  var $textArea = null;
+  var $markdown = null;
+
+$(document).ready(function() {
+  // Using the default widget.
+  $textArea = $(rText.widget());
+  $markdown = $('#markdown');
+
+  $('#text').append($textArea);
+
+  // TODO: find a way to make this evented.
+  // Run an initial render after the text area has time to populate.
+  setTimeout(updateMarkdown, 250);
+});
+
+function updateMarkdown() {
+  $markdown.html(marked($textArea.val()));
 };
-// $textArea.on('change', updateMarkdown);
-setInterval(updateMarkdown, 10);
-$('#markdown').html(marked($textArea.val()));
-// $('#markdown').append(widget(rText));
